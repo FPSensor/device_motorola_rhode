f872acca4872b880b903d06099a532876551b124
FPSensor <gkartyt@gmail.com>
2024-06-22 08:05:46 +0000

    rhode: XPe initial bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/xperience_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rhode-user \
-    lineage_rhode-userdebug \
-    lineage_rhode-eng
+    xperience_rhode-user \
+    xperience_rhode-userdebug \
+    xperience_rhode-eng
--- xperience_rhode.mk
+++ xperience_rhode.mk
@@ -11,11 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common The Xperience Project stuff.
+$(call inherit-product, vendor/xperience/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+XPERIENCE_MAINTAINER := FPSensor
+XPERIENCE_CHIPSET := "sm6225"
+XPERIENCE_BATTERY := 5000mAh
+XPERIENCE_DISPLAY := 1080x2400
+TARGET_STOCK_GAPPS := true
+WITH_GMS := true
+TARGET_ENABLE_PIXEL_GOODIES := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_rhode
+PRODUCT_NAME := xperience_rhode
 PRODUCT_DEVICE := rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
